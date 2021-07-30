4aad5ea809ede698c6fef3276f23427ad30c61c5
shuibcodes <abdullahshuib124@gmail.com>
2021-07-30 20:41:41 +0100

    PLS;2C

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import {
   useHistory
 } from "react-router-dom";
 import Article from '../src/Article'
+import Card from './Card'
 function App() {
 
   const [isOpen, setIsOpen] = useState(false)
@@ -34,6 +35,24 @@ function App() {
 
 // }, []);
 
+const [blogs, setBlogs] = useState([])
+const [image, setImage] = useState()
+const [selectedBlog, setSelectedBlog] = useState(blogs)
+
+useEffect(() => {
+    fetch("http://cdn.contentful.com/spaces/ns6ic3hnqqep/environments/master/entries?access_token=dDIeVUdKolVdP8jlmXjiNdZwwFCcYSXrxy3OMo-NXN8")
+    .then(response => response.json())
+    .then(data =>
+      setBlogs(data.items)
+   
+    )
+  }, []) 
+            //  get image
+
+  
+
+  // blogs.map((d)=> console.log(d))
+  console.log(blogs)
   return (
     <>
         <Navbar toggle={toggle} />
@@ -51,12 +70,28 @@ function App() {
           </Route>
  
         </Switch>
-     
+
+
+        <div>
+      <div>
+        {selectedBlog !== null ? blogs.map((blog =>
+          <div  className="grid"> 
+          <Card title={blog.fields.title} introduction={blog.fields.introduction} mainBody1={blog.fields.Body} mainBody2={blog.fields.mainBody2} setSelectedBlog={selectedBlog} />  
+          </div>
+
+
+      
+        )): 
+            <Article title={blogs.find(d => d.fields.title === selectedBlog)}   />
+        }
+        
+       </div>
+        </div>
 
 
 
     </>
-  );
+  )
 }
 
 export default App;
